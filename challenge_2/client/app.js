fc9472915ee8fd0645a16191281d7e9bc8f64d71
AbobkerElaghel <abobkerelaghel@gmail.com>
2019-12-30 10:27:16 +0100

    callange 2 to finished yet

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,26 @@
 
+function sendDate(e){
+    e.preventDefault();
+    $.ajax({
+        url: 'http://localhost:3000/',
+        type: 'POST',
+        data: $('JSO').value,
+        contentType: 'text/html',
+        success: console.log('الحبببب'),
+        
+      });
+}
 
+$('#sumbitButten').on('click',sendDate);
+
+function getData(e){
+    e.preventDefault();
+    $.ajax({
+        url: 'http://localhost:3000/',
+        type: 'POST',
+        data: $('JSO').value,
+        contentType: 'text/html',
+        success: console.log('الحبببب'),
+        
+      });
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,64 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+var urlencodedParser = require('urlencoded-parser');
+
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json())
+app.use( express.static(__dirname + '/../client') );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(urlencodedParser);
+
+var path = 'C:\\Users\\DELL I5\\Desktop\\MINIAPPS\\rbktn01-mini-apps-1\\challenge_2\\client\\index.html'
+
+app.get('/', (req, res) => {
+    res.sendfile(path);
+ })
 
 
+app.post('/', (req, res) => {
+   var object = ( JSON.parse(req.body.JSO) );
+   var csvType = convertToCSV(object);
+   
+   console.log(csvType)
+    res.end();
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+function convertToCSV(jsonObject){
+    let resultStr = '';
+    let keys = Object.keys(jsonObject);
+    keys.pop();
+
+
+    for (let i = 0; i < keys.length; i++) {
+            resultStr+=`${keys[i]},`;
+    }
+    resultStr = resultStr.slice(0,resultStr.length-1);
+    resultStr+='\n';
+
+    function getAllValue(e){
+        let values = Object.values(e);
+        var temp;
+
+        for (let i = 0; i < values.length; i++) {
+            if (!(typeof values[i] === 'object')) {
+                resultStr+=`${values[i]},`;
+            }else {
+                temp = values[i];
+            }
+        }
+        resultStr = resultStr.slice(0,resultStr.length-1);
+        resultStr+='\n';
+
+        if(temp){
+        for (let i = 0; i < temp.length; i++) {
+            getAllValue(temp[i]);
+            }
+        }
+    }
+    getAllValue(jsonObject);
+    resultStr = resultStr.slice(0,resultStr.length-1);
+    return resultStr;
+}
